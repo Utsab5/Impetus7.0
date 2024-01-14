2ddec0a60a8b8d1d8b0d2ffc61afc51c72ba82a5
Utsab Kundu <kunutsab@gmail.com>
2024-01-15 02:33:58 +0530

    fixed deployment issue and added forms

--- pages/events/cadathon.js
+++ pages/events/cadathon.js
@@ -128,7 +128,7 @@ Engineering, IIEST Shibpur under the banner of &quot;Impetus 7.0&quot;.
               type="button"
               className="mt-2 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
               onClick={(event) =>
-                (window.location.href = "https://forms.gle/NRdSf7w4pvX5E96y7")
+                (window.location.href = "https://forms.gle/pxEokiDjCRBXDMah7")
               }
             >
               Register Now
@@ -325,7 +325,7 @@ choice chosen by the participants through Google form.
                 type="button"
                 className=" font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
                 onClick={(event) =>
-                  (window.location.href = "https://forms.gle/NRdSf7w4pvX5E96y7")
+                  (window.location.href = "https://forms.gle/pxEokiDjCRBXDMah7")
                 }
               >
                 Register Now
--- pages/events/valorant.js
+++ pages/events/valorant.js
@@ -126,7 +126,7 @@ export default function CadathonPage() {
               type="button"
               className="mt-2 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
               onClick={(event) =>
-                (window.location.href = "https://forms.gle/NRdSf7w4pvX5E96y7")
+                (window.location.href = "https://forms.gle/8J18pVrNTjMNBD399")
               }
             >
               Register Now
@@ -378,7 +378,7 @@ export default function CadathonPage() {
                 type="button"
                 className=" font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
                 onClick={(event) =>
-                  (window.location.href = "https://forms.gle/NRdSf7w4pvX5E96y7")
+                  (window.location.href = "https://forms.gle/8J18pVrNTjMNBD399")
                 }
               >
                 Register Now
