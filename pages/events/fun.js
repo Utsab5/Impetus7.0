d8613fce80a6cb59e6477d3959b4bb66046c6688
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-01-31 20:59:40 +0530

    fixed

--- pages/events/fun.js
+++ pages/events/fun.js
@@ -81,7 +81,7 @@ export default function CadathonPage() {
     },
   };
   return (
-    <Box sx={{ width: "100vw", background:"black" }} className="center1 mt-10">
+    <Box sx={{ width: "100vw", background: "black" }} className="center1 mt-10">
       <Box sx={headerStyles} className="center2">
         <IconButton onClick={goBack} className="left">
           <ArrowBackIcon fontSize="large" color="white" />
@@ -99,45 +99,49 @@ export default function CadathonPage() {
             event. If you can manage all these , some prizes are surely waiting
             for you.
           </Typography>
-          <br></br><br></br>
-          <h1 className="md:text-3xl text-2xl font-bold ">
-            Balloon Dart Game 
-          </h1>
+          <br></br>
+          <br></br>
+          <h1 className="md:text-3xl text-2xl font-bold ">Balloon Dart Game</h1>
           <Typography variant="p">
-          A popular and engaging funfair game that blends excitement and accuracy. Using a set of darts, participants must burst balloons attached to a backdrop.  The challenge lies in using the dart's sharp point to accurately hit and pop the balloons, each of which may conceal a reward.<br></br>
+            A popular and engaging funfair game that blends excitement and
+            accuracy. Using a set of darts, participants must burst balloons
+            attached to a backdrop. The challenge lies in using the dart&apos;s
+            sharp point to accurately hit and pop the balloons, each of which
+            may conceal a reward.<br></br>
             <span className="py-3 px-2.5 me-2  md:text-lg text-md font-medium rounded-lg text-center  bg-gray-800 text-gray-300  ">
               {" "}
               Venue - Parade Ground, IIEST Shibpur.
             </span>
           </Typography>
 
-          <br></br><br></br>
-          <h1 className="md:text-3xl text-2xl font-bold ">
-          Buzz Wire Game
-          </h1>
+          <br></br>
+          <br></br>
+          <h1 className="md:text-3xl text-2xl font-bold ">Buzz Wire Game</h1>
           <Typography variant="p">
-          A captivating and challenging activity that requires a steady hand and precise coordination. The game involves guiding a metal loop along an electrified wire, avoiding contact with the wire. If the loop gets in contact with the wire, it triggers a buzzing sound, and the player wins.<br></br>
+            A captivating and challenging activity that requires a steady hand
+            and precise coordination. The game involves guiding a metal loop
+            along an electrified wire, avoiding contact with the wire. If the
+            loop gets in contact with the wire, it triggers a buzzing sound, and
+            the player wins.<br></br>
             <span className="py-3 px-2.5 me-2  md:text-lg text-md font-medium rounded-lg text-center  bg-gray-800 text-gray-300  ">
               {" "}
               Venue - Parade Ground, IIEST Shibpur.
             </span>
           </Typography>
 
-
-          <br></br><br></br>
-          <h1 className="md:text-3xl text-2xl font-bold ">
-          Robosoccer
-          </h1>
+          <br></br>
+          <br></br>
+          <h1 className="md:text-3xl text-2xl font-bold ">Robosoccer</h1>
           <Typography variant="p">
-          Participants engage in a thrilling one-on-one competition where each player is provided with robots. Two players compete in a game where they control robots on a field, aiming to skillfully navigate, strategically dribble the soccer ball, and score goals.<br></br>
+            Participants engage in a thrilling one-on-one competition where each
+            player is provided with robots. Two players compete in a game where
+            they control robots on a field, aiming to skillfully navigate,
+            strategically dribble the soccer ball, and score goals.<br></br>
             <span className="py-3 px-2.5 me-2  md:text-lg text-md font-medium rounded-lg text-center  bg-gray-800 text-gray-300  ">
               {" "}
               Venue - Parade Ground, IIEST Shibpur.
             </span>
           </Typography>
-
-
-
         </Box>
       </Box>
       <Contact />
